6c6cfbc4876813ac4a6df86a95cb260f5c2d1c6a
chris wren <chriswrendeveloper@gmail.com>
2025-05-19 17:18:25 +0100

    Setup Project

---
+++ src/bin/kvs.rs
@@ -0,0 +1,50 @@
+use clap::{Command, arg};
+use std::process;
+
+fn cli() -> Command {
+    Command::new(env!("CARGO_PKG_NAME"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("set")
+                .about("Add a key/value to the store")
+                .arg(arg!(<KEY> "The key added to store"))
+                .arg(arg!(<VALUE> "The value added to store"))
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("get")
+                .about("Retrieve a value from the store using a key")
+                .arg(arg!(<KEY> "The key in the store"))
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("rm")
+                .about("Remove a key/value pair from store")
+                .arg(arg!(<KEY> "The key in the store"))
+                .arg_required_else_help(true),
+        )
+}
+
+fn main() {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("set", _matches)) => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        }
+        Some(("get", _matches)) => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        }
+        Some(("rm", _matches)) => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        }
+        _ => unreachable!(),
+    }
+}
