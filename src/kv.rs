fdab6c9503d501b165a656908ffaed5136115972
chris wren <chriswrendeveloper@gmail.com>
2025-06-05 09:52:06 +0100

    Added logic to build index on startup

--- src/kv.rs
+++ src/kv.rs
@@ -3,8 +3,9 @@
 
 use std::collections::HashMap;
 
+use std::fs;
 use std::fs::{File, OpenOptions};
-use std::io::{BufReader, BufWriter, Error, Read, Seek, SeekFrom, Write};
+use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 
 use crate::{KvsError, Result};
@@ -30,7 +31,7 @@ struct Segment {
 impl Segment {
     fn new(dir_path: &Path, file_id: u64) -> Result<Segment> {
         // Create empty file
-        let file_name = format!("log.{}", file_id);
+        let file_name = format!("{}.log", file_id);
         let path = dir_path.join(file_name);
 
         // Write handle
@@ -58,7 +59,7 @@ impl Segment {
     }
     fn open(dir_path: &Path, file_id: u64, status: SegmentStatus) -> Result<Segment> {
         // Create empty file
-        let file_name = format!("log.{}", file_id);
+        let file_name = format!("{}.log", file_id);
         let path = dir_path.join(file_name);
 
         // Write handle
@@ -83,6 +84,12 @@ impl Segment {
     }
     fn read(&mut self, offset: u64, length: u64) -> Result<Vec<u8>> {
         // Get key-value at a given offset (provided by index)
+
+        let file_len = self.reader.get_ref().metadata()?.len();
+        println!(
+            "Offset: {}, Length: {}, File size: {}",
+            offset, length, file_len
+        );
 
         let mut buffer = vec![0; length as usize];
 
@@ -140,6 +147,74 @@ impl Segment {
             length: value_bytes.len() as u64,
         })
     }
+    fn index(&mut self, index: &mut HashMap<String, CommandPos>) -> Result<u32> {
+        let mut offset = 0;
+        let mut stale_entries = 0;
+        let file_size = self.reader.get_ref().metadata()?.len();
+
+        // loop through file
+        loop {
+            if offset + 16 > file_size {
+                break; // not enough for header
+            }
+
+            // Get key/value size
+            let key_size_bytes: [u8; 8] = self
+                .read(offset, 8)?
+                .try_into()
+                .expect("Expected exactly 8 bytes");
+            let key_size = u64::from_le_bytes(key_size_bytes);
+
+            offset += 8;
+
+            let value_size_bytes: [u8; 8] = self
+                .read(offset, 8)?
+                .try_into()
+                .expect("Expected exactly 8 bytes");
+            let value_size = u64::from_le_bytes(value_size_bytes);
+
+            offset += 8;
+
+            if offset + key_size + value_size > file_size {
+                break; // incomplete entry
+            }
+
+            // Get key value
+            let key_bytes = match self.read(offset, key_size) {
+                Ok(bytes) => bytes,
+                _ => break, // EOF or read error
+            };
+
+            let key_value = match String::from_utf8(key_bytes) {
+                Ok(s) => s,
+                Err(_) => break, // Skip invalid UTF-8 key
+            };
+
+            // Update offset to start of value
+            offset += key_size;
+
+            // Handle removed key/value pairs (tombstone value)
+            if value_size == 0 {
+                stale_entries += 1;
+                continue;
+            }
+
+            // Update index
+            index.insert(
+                key_value,
+                CommandPos {
+                    file_id: self.file_id,
+                    offset,
+                    length: value_size,
+                },
+            );
+
+            // Update offset to start of next entry
+            offset += value_size;
+        }
+
+        Ok(stale_entries)
+    }
     fn size(&self) -> Result<u64> {
         // Measure if compaction is needed
         let file_ref = self.reader.get_ref();
@@ -147,8 +222,9 @@ impl Segment {
         let size = metadata.len();
         Ok(size)
     }
-    fn compact() {
+    fn compact(&mut self) {
         // Remove stale entries
+        self.status = SegmentStatus::Compacting;
     }
 }
 
@@ -187,24 +263,76 @@ impl KvStore {
         let mut segments = HashMap::new();
 
         // Create index
-        let index = HashMap::new();
+        let mut index = HashMap::new();
 
-        // Temp - Single File
-        let file_id = 1;
-        let active = file_id.to_owned().to_string();
-        let segment = Segment::new(&base_dir, file_id)?;
-        segments.insert(active.clone(), segment);
-
+        // Get all files
         // Check directory for log files
+        let mut file_ids = fs::read_dir(dir_path)?
+            .filter_map(|entry| {
+                let path = entry.ok()?.path();
+
+                if path.extension()?.to_str()? != "log" {
+                    return None;
+                }
+
+                let stem_str = path.file_stem()?.to_str()?;
+                stem_str.parse::<u64>().ok()
+            })
+            .collect::<Vec<u64>>();
+
+        file_ids.sort_unstable();
+
         // Create segment for each log file
-        // Update index with segment
+        let mut active: String = file_ids.iter().copied().max().unwrap_or(0).to_string();
+
+        // Calculate size of log
+        let mut size = 0;
+
+        // loop newest to oldest (highest to lowest)
+        for id in file_ids {
+            // Determine status
+            let status = if id.to_string() == active {
+                SegmentStatus::Active
+            } else {
+                SegmentStatus::Archived
+            };
+
+            // Create segment for file
+            let mut segment = Segment::open(dir_path, id, status)?;
+
+            // Update index with segment
+            let stale_entries = segment.index(&mut index)?;
+
+            // Count stale entries (rm, duplicate)
+            match segment.status {
+                SegmentStatus::Archived if stale_entries > 0 => {
+                    // stale entires = sealed
+                    segment.status = SegmentStatus::Sealed;
+                }
+                _ => {}
+            }
+
+            // update log size
+            size += segment.size;
+
+            // Add segment to segments hashmap
+            segments.insert(id.to_string(), segment);
+        }
+
+        // If no files, create one
+        if segments.is_empty() {
+            let file_id = 1;
+            active = file_id.to_owned().to_string();
+            let segment = Segment::new(&base_dir, file_id)?;
+            segments.insert(active.clone(), segment);
+        }
 
         // Create Log
         Ok(KvStore {
             base_dir,
             segments,
             active,
-            size: 0,
+            size,
             index,
         })
     }
@@ -265,20 +393,21 @@ impl KvStore {
         // exit code 0
 
         // Get log pointer from index
-        let log_pointer = self.index.get(&key).ok_or(KvsError::KeyNotFound)?;
+        let log_pointer = match self.index.get(&key) {
+            Some(ptr) => ptr,
+            None => return Ok(None),
+        };
 
         let file_id = log_pointer.file_id.to_string();
 
-        let segment = self
-            .segments
-            .get_mut(&file_id)
-            .ok_or(KvsError::FileNotFound)?;
+        let segment = match self.segments.get_mut(&file_id) {
+            Some(seg) => seg,
+            None => return Ok(None),
+        };
 
         // Has all the data (kv length, val length, key, value)
         let value_bytes = segment.read(log_pointer.offset, log_pointer.length)?;
         let value = String::from_utf8_lossy(&value_bytes).into_owned();
-
-        println!("VALUE: {}", value);
 
         Ok(Some(value))
     }
@@ -293,8 +422,12 @@ impl KvStore {
         // -- append to log
         // -- if successful exit silently / exit code 0
         // -- if failure print error / return non-zero code
-        self.set(key.clone(), "".to_string())?;
         self.index.remove(&key).ok_or(KvsError::KeyNotFound)?;
+        self.set(key.clone(), "".to_string())?;
         Ok(())
+    }
+    /// Size of log
+    pub fn size(self) -> u64 {
+        self.size
     }
 }
