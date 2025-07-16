512df982686cb43056519fa497ac793c802a8a29
chris wren <chriswrendeveloper@gmail.com>
2025-07-16 09:19:00 +0100

    Added once_cell to cache benchmark values

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,6 +1,7 @@
 use clap::ValueEnum;
 use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
 use kvs::{Engine, Storage};
+use once_cell::sync::Lazy;
 use rand::{Rng, SeedableRng, distributions::Alphanumeric, rngs::SmallRng};
 use tempfile::TempDir;
 
@@ -14,8 +15,8 @@ const READ_SEED: u64 = 999;
 fn get_size(seed: u64) -> [usize; NUM_VALS] {
     let mut r: SmallRng = SeedableRng::seed_from_u64(seed);
     let mut res = [0; NUM_VALS];
-    for i in 0..NUM_VALS {
-        res[i] = r.gen_range(1, 100_000);
+    for val in res.iter_mut() {
+        *val = r.gen_range(1, 100_000);
     }
     res
 }
@@ -30,15 +31,17 @@ fn get_vals(seed: u64, size: &[usize]) -> Vec<String> {
     res
 }
 
+static KEY_SIZES: Lazy<[usize; NUM_VALS]> = Lazy::new(|| get_size(KEY_SIZE_SEED));
+static VAL_SIZES: Lazy<[usize; NUM_VALS]> = Lazy::new(|| get_size(VALUE_SIZE_SEED));
+
+static KEYS: Lazy<Vec<String>> = Lazy::new(|| get_vals(KEY_SEED, &*KEY_SIZES));
+static VALS: Lazy<Vec<String>> = Lazy::new(|| get_vals(VALUE_SEED, &*VAL_SIZES));
+
 fn bench_write(c: &mut Criterion) {
-    let key_sizes = get_size(KEY_SIZE_SEED);
-    let keys = get_vals(KEY_SEED, &key_sizes);
-
-    let val_sizes = get_size(VALUE_SIZE_SEED);
-    let vals = get_vals(VALUE_SEED, &val_sizes);
-
     for engine in Engine::value_variants() {
         let mut bench_write = c.benchmark_group("bench_write");
+        let keys = &KEYS;
+        let vals = &VALS;
 
         let engine_name = engine.to_string();
         let write_id = format!("{}-write", engine_name);
@@ -62,14 +65,11 @@ fn bench_write(c: &mut Criterion) {
 }
 
 fn bench_read(c: &mut Criterion) {
-    let key_sizes = get_size(KEY_SIZE_SEED);
-    let keys = get_vals(KEY_SEED, &key_sizes);
-
-    let val_sizes = get_size(VALUE_SIZE_SEED);
-    let vals = get_vals(VALUE_SEED, &val_sizes);
-
     for engine in Engine::value_variants() {
         let mut bench_read = c.benchmark_group("bench_read");
+
+        let keys = &KEYS;
+        let vals = &VALS;
 
         let engine_name = engine.to_string();
         let read_id = format!("{}-read", engine_name);
